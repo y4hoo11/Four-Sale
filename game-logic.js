c8a782bc1a73dff1538e55c21129d0d099b63e69
y4hoo11 <yahho9049@gmail.com>
2026-06-12 09:53:11 +0900

    シャッフルのコード修正

--- game-logic.js
+++ game-logic.js
@@ -311,7 +311,7 @@ class FourSaleGame {
     shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[array[j]]] = [array[array[j]], array[i]];
+            [array[i], array[j]] = [array[j], array[i]];
         }
     }
 }
